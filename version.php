32a7874b7cb536af440028aff605d7c9e0f88af4
jazinheira <jazinheira@users.noreply.github.com>
2017-10-24 16:12:52 -0400

    Update plugin version

--- version.php
+++ version.php
@@ -36,8 +36,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$plugin->version  = 2013092204;
-$plugin->release = '1.0 (Build: 2013071500)';
-$plugin->maturity = MATURITY_STABLE;
+$plugin->version  = 2017102400;
 $plugin->requires = 2016120500;
+$plugin->release = '1.0.1';
+$plugin->maturity = MATURITY_BETA;
 $plugin->component = 'local_sayonara';
